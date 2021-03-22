1f6461ab28277059443d38e324377745007317e3
Nate M King <72567521+natemking@users.noreply.github.com>
2021-03-22 12:37:22 -0400

    Slider JS completed

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,7 +1,8 @@
 const listEl = document.querySelector('ul');
 const thermometerEl = document.querySelector('.thermometer');
+const meterEl = document.querySelector('.meter')
 const mercuryEl = document.querySelector('.mercury');
-
+const sliderEl = document.querySelector('.slider');
 const url = window.location.href;
 const darkBlue = 'rgb(38, 140, 195)';
 const lightBlue = 'rgb(148, 207, 238)'
@@ -35,9 +36,46 @@ switch (true) {
  */
 mercuryEl.setAttribute('id', 'mercury');
 
+
 for (let i = 1; i <= 3; i++) {
     let mercuryElClone = mercuryEl.cloneNode(true);
-    mercuryElClone.setAttribute('id', `mercury-clone${i}`);
+    mercuryElClone.setAttribute('id', `mercuryClone${i}`);
     thermometerEl.append(mercuryElClone);
+
+
+    this[`mercuryElClone${i}`] = document.querySelector(`#mercuryClone${i}`);
+
 }
+
+meterEl.addEventListener('click', (e) => {
+    
+    switch (e.target.id) {
+        case ('setTo1'):
+            mercuryElClone1.classList.remove('mercuryFilled');
+            mercuryElClone2.classList.remove('mercuryFilled');
+            mercuryElClone3.classList.remove('mercuryFilled');
+            sliderEl.setAttribute('style', 'left: 10.5%');
+            break;
+        case ('setTo2'):
+            mercuryElClone1.classList.add('mercuryFilled');
+            mercuryElClone2.classList.remove('mercuryFilled');
+            mercuryElClone3.classList.remove('mercuryFilled');
+            sliderEl.setAttribute('style', 'left: 35%');
+            break;
+        case ('setTo3'):
+            mercuryElClone1.classList.add('mercuryFilled');
+            mercuryElClone2.classList.add('mercuryFilled');
+            mercuryElClone3.classList.remove('mercuryFilled');
+            sliderEl.setAttribute('style', 'left: 59.5%');
+            break;
+        case ('setTo4'):
+            mercuryElClone1.classList.add('mercuryFilled');
+            mercuryElClone2.classList.add('mercuryFilled');
+            mercuryElClone3.classList.add('mercuryFilled');
+            sliderEl.setAttribute('style', 'left: 85%');
+            break;
+        default:
+            break;
+    }
+})
 
